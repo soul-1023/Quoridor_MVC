cb0330c54263d4ac4160b4e33cb35041b0a02387
soul-1023 <soul_1023@mail.ru>
2021-05-22 23:34:39 +0200

    Написали базовую реализацию метода, который устанавливает для каждого персонажа сторону, до которой он должен добраться. Добавили реализацию AbstractCharactersManager.

---
+++ Controllers/AbstractCharactersManager.cs
@@ -0,0 +1,57 @@
+﻿using Quoridor_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Quoridor_MVC.Controllers
+{
+    abstract class AbstractCharactersManager
+    {
+        public AbstractCharacter[] Characters { get; protected set; }
+
+        public Dictionary<AbstractCharacter, Coords[]> WinPositions;
+
+        protected Coords[] SetWinningSide(Coords startPositionOfCharacter, int sizeOfGraphSide)
+        {
+            Coords[] coords = new Coords[sizeOfGraphSide];
+            bool isTopSide = startPositionOfCharacter.y == 0;
+            bool isRightSide = startPositionOfCharacter.x == sizeOfGraphSide - 1;
+            bool isBottomSide = startPositionOfCharacter.y == sizeOfGraphSide - 1;
+            bool isLeftSide = startPositionOfCharacter.x == 0;
+
+            if (isTopSide)
+            {
+                return coords.Select((_, i) => new Coords(i, sizeOfGraphSide - 1)).ToArray();
+            } 
+            else if (isRightSide)
+            {
+                return coords.Select((_, i) => new Coords(0, i)).ToArray();
+            } 
+            else if (isBottomSide)
+            {
+                return coords.Select((_, i) => new Coords(i, 0)).ToArray();
+            } 
+            else if (isLeftSide)
+            {
+                return coords.Select((_, i) => new Coords(sizeOfGraphSide - 1, i)).ToArray();
+            }
+
+            return null;
+        }
+
+        public void MixPlayers()
+        {
+            
+        }
+
+        protected abstract AbstractCharactersManager SwitchTurn();
+
+        protected abstract AbstractCharacter GetActiveCharacter();
+
+        protected abstract bool MoveCharacter(AbstractGraph graph, params Coords[] coords);
+
+        protected abstract bool CreatePlayer(string name, Coords startPosition);
+
+        protected abstract bool CreateAI(string name, Coords startPosition);
+    }
+}
---
+++ Controllers/CharactersManager.cs
@@ -0,0 +1,37 @@
+﻿using Quoridor_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quoridor_MVC.Controllers
+{
+    class CharactersManager : AbstractCharactersManager
+    {
+        protected override bool CreateAI(string name, Coords startPosition)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool CreatePlayer(string name, Coords startPosition)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override AbstractCharacter GetActiveCharacter()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool MoveCharacter(AbstractGraph graph, params Coords[] coords)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override AbstractCharactersManager SwitchTurn()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
